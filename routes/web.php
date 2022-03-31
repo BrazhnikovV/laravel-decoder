02f6b46f182b6a5ceee690e198ba46046260a838
Vasilii <brazhnikov_ve@mail.ru>
2022-03-31 12:02:34 +0300

    Добавил маршрут и представление для сьраницы логина.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', static function () {
     return view('welcome');
 });
+
+Route::get('/login', static function () {
+    return view('login.login');
+});
